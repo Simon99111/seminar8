3203d8dc64453b0d90fc2f9cacccd202fe9e0880
Antony <antua.froloff@gmail.com>
2023-03-05 15:19:28 +0300

    userInput

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@ Console.WriteLine("Hello, World!");
 
 
 //Задать двумерный массив
-void SetArray(int x, int y)
+static void SetArraySize()
 {
+    int x, y;
+    Console.WriteLine("Enter size of the rows: ");
+    x = Convert.ToInt32(Console.ReadLine());
+
+
+    Console.WriteLine("Enter size of the colomns: ");
+    y = Convert.ToInt32(Console.ReadLine());
+
     int[,] array = new int[x, y];
 }
