4fe320d270b2613cb739d511cf1c2b5fb6a014c6
Yuliya-X <124186944+Yuliya-X@users.noreply.github.com>
2023-03-05 14:45:30 +0300

    Заполнение случайными числами

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// 2. Заполнение случайными числами (1,10) (FillArray) - Юлия
+
+int[,] FillArray(int m, int n) => new int[m, n];
+
+void FillMatrix(int [,] mat, int min = 1, int max = 10)
+{
+  int row = mat.GetLength(0);
+  int columns = mat.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+        mat[i, j] = new Random().Next(min, max);
+        }
+    }
+}
