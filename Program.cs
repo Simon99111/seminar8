b1cc7baee561d5e298675bbd99ef013f9a74c990
Antony <antua.froloff@gmail.com>
2023-03-05 14:42:56 +0300

    задать двум. массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+//Задать двумерный массив
+void SetArray(int x, int y)
+{
+    int[,] array = new int[x, y];
+}
